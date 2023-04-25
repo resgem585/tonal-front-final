5403160affd31034474631c68968ae37cbded1bd
Rodolfo <resgem585@gmail.com>
2023-04-24 21:10:38 -0600

    primer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Router, Routes, Route } from 'react-router-dom'
+import { Login } from './Pages/Login'
+import LoginForm from './components/LoginForm'
+
+function App() {
+  
+
+  return (
+   <>
+   <LoginForm />
+   </>
+  )
+}
+
+export default App
