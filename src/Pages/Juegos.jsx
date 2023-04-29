99142d597052d158729ea30be53171c118b919f4
Rodolfo <resgem585@gmail.com>
2023-04-28 23:00:42 -0600

    Portada/juegos

--- src/Pages/Juegos.jsx
+++ src/Pages/Juegos.jsx
@@ -1,8 +1,12 @@
+import About from "../components/About/About";
 import JuegoCard from "../components/JuegoCard";
 
 const Juegos = () => {
   return (
+    <>
+    <About />
     <JuegoCard />
+    </>
   )
 }
 export default Juegos
---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+// Style
+import './About.scss'
+
+const About = () => {
+  return (
+    <section className='base'>
+      <div className='base__about'>
+        <div className='base__about__text'>
+          <span className='base__about__text--title'>Juegos </span>
+          <span className='base__about__text--subtitle'>El patolli </span>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
