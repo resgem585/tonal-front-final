b59e3e7abbe766c53934d4e606ad87c02635eec5
Victoria Trejo <victoriatrejocs@gmail.com>
2023-05-11 13:23:13 -0600

    introducción y cards

--- src/layouts/TitleJuego/TitleJuego.jsx
+++ src/layouts/TitleJuego/TitleJuego.jsx
@@ -6,8 +6,8 @@ const TitleJuego = () => {
     <section className='base'>
       <div className='base__about'>
         <div className='base__about__text'>
-          <span className='base__about__text--title'>Juegos </span>
-          <span className='base__about__text--subtitle'>El patoll </span>
+          <span className='base__about__text--title'>Juegos prehispánicos</span>
+          <span className='base__about__text--subtitle'>Símbolo de vida, muerte y reencarnación</span>
         </div>
       </div>
     </section>
