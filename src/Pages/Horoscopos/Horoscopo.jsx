3b74f8125da472eaad90accbd8e2b9f823062a04
Victoria Trejo <victoriatrejocs@gmail.com>
2023-05-16 00:38:43 -0600

    artículo mono

---
+++ src/Pages/Horoscopos/Horoscopo.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/General/Navbar"
+import Footer from "../../components/General/Footer"
+import HorosInput from "../../components/Horoscopo/HorosInput"
+
+const Horoscopo = () => {
+    return (
+      <>
+      <Navbar/>
+      <HorosInput/>
+      <Footer/>
+      </>
+    )
+  }
+  export default Horoscopo
---
+++ src/components/Horoscopo/MonoArticulo.jsx
@@ -0,0 +1,177 @@
+const Mono = () => {
+    return (
+      <article className="max-w-screen-lg mx-auto px-4 py-8">
+
+<section>
+  <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+    <header className="text-center">
+      <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+      Tu signo azteca: Ozomahtli, mono
+      </h2>
+
+      <p className="max-w-md mx-auto mt-4 text-gray-500">
+      Tu fecha de nacimiento: 18 de mayo
+      </p>
+    </header>
+
+    <ul className="grid grid-cols-1 gap-4 mt-8 lg:grid-cols-3">
+      <li>
+        <a href="#" className="relative block group">
+          <img
+            src="https://www.redaccionmedica.com/images/destacados/china-notifica-la-primera-muerte-de-un-humano-por-el-virus-del-mono-b-4471.jpg"
+            alt=""
+            className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+          />
+
+          <div
+            className="absolute inset-0 flex flex-col items-start justify-end p-6"
+          >
+            <h3 className="text-xl font-medium text-white">Mono</h3>
+          </div>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" className="relative block group">
+          <img
+            src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/psychedelic-aztec-god-xochipilli-nikolay-todorov.jpg"
+            alt=""
+            className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+          />
+
+          <div
+            className="absolute inset-0 flex flex-col items-start justify-end p-6"
+          >
+            <h3 className="text-xl font-medium text-white">Acompañante: Xochipilli</h3>
+
+          </div>
+        </a>
+      </li>
+
+      <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+        <a href="#" className="relative block group">
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/58274743e6f2e13993af0ae3/1605067310354-7XHBF1GVLCN34P9JME86/Monkey-Sticker.png?format=1000w"
+            alt=""
+            className="object-cover w-full transition duration-500 aspect-square group-hover:opacity-90"
+          />
+
+          <div
+            className="absolute inset-0 flex flex-col items-start justify-end p-6"
+          >
+            <h3 className="text-3xl font-medium">Ozomahtli</h3>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
+
+        <p className="font-bold text-xl mt-8 mb-4"></p>
+        <div className="prose">
+          <div className="mb-8">
+
+          </div>
+          <h2 className="font-bold text-xl mt-8 mb-4">Descripción</h2>
+      <p>
+        Ozomahtli es el nombre genérico para todas las especies de monos conocidos en México. Se le asocia con la diversión y la alegría debido a su gracia y agilidad de movimientos. Los nacidos en Ozomahtli tienden a ser personas muy alegres, divertidas y ocurrentes. Sin embargo, pueden ser un poco superfluos y no dar importancia a lo trascendente a su alrededor.
+      </p>
+          <div className="mb-8">
+          <h2 className="font-bold text-xl mt-8 mb-4">Acompañante: Xochipilli</h2>
+      <p>
+        Xochipilli, en la cultura mexica es el dios del amor, los juegos, la belleza, las flores, el maíz, el placer y de la ebriedad sagrada. Literalmente "El hijo de las flores" o "Lo que cuelga de las flores", se refiere al efecto de la luz del sol sobre las plantas y su función en los seres vivos. En una persona, representa el apego hacia la botánica y la facilidad para descubrir los efectos de las plantas en el ser humano. Quienes tienen a Xochipilli como acompañante son personas muy creativas y pueden expresar sus conocimientos a través de la pintura, el teatro o la danza.
+      </p>
+          </div>
+
+
+          <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2">
+  <article className="rounded-xl bg-gradient-to-r from-red-900 via-red-500 to-yellow-300 p-0.5 shadow-xl transition hover:shadow-sm">
+      <div className="bg-white p-4 sm:p-6">
+        <a href="#">
+          <h2 className="font-bold text-xl mt-8 mb-4 text-center">
+          Características del signo Mono
+        </h2>
+      </a>
+      <ul>
+        <li>Filosófico, misterioso e imaginativo.</li>
+        <li>Ve más allá de los cinco sentidos y confía en su instinto y voz interior.</li>
+        <li>Expresivo, transmite alegría y confianza.</li>
+        <li>Proyecta sensualidad y disfruta de la diversión.</li>
+        <li>Puede caer víctima de su propia curiosidad.</li>
+        <li>Alcanza muchas cosas gracias a su agilidad.</li>
+      </ul>
+    </div>
+  </article>
+  
+
+  <article className="rounded-xl bg-gradient-to-r from-red-900 via-red-500 to-yellow-300 p-0.5 shadow-xl transition hover:shadow-sm">
+      <div className="bg-white p-4 sm:p-6">
+        <a href="#">
+          <h2 className="font-bold text-xl mt-8 mb-4 text-center">
+          Relaciones y amor
+        </h2>
+      </a>
+      <ul>
+        <li>Amigo generoso y útil.</li>
+        <li>Egoísta en el amor y no le gusta someterse a restricciones.</li>
+        <li>Más sentimental que carnal en su sexualidad.</li>
+        <li>Preferencia por la charla en pareja en lugar de pasiones turbulentas.</li>
+        <li>Aprecio por la naturaleza y asume los golpes por distracción.</li>
+      </ul>
+    </div>
+  </article>
+
+
+  <article className="rounded-xl bg-gradient-to-r from-red-900 via-red-500 to-yellow-300 p-0.5 shadow-xl transition hover:shadow-sm">
+      <div className="bg-white p-4 sm:p-6">
+        <a href="#">
+          <h2 className="font-bold text-xl mt-8 mb-4 text-center">
+        Personalidad y comportamiento
+        </h2>
+      </a>
+      <ul>
+      <li>Pensamiento orientado hacia la comunidad y brinda ayuda de buena gana.</li>
+        <li>Expresan y comparten con generosidad lo que les pertenece y poseen.</li>
+        <li>Siempre resultarán simpáticos.</li>
+        <li>Pueden ser indecisos y posponer elecciones.</li>
+        <li>Distraídos con sus propias cosas, pero no lo son con los demás y sus asuntos.</li>
+      </ul>
+    </div>
+  </article>
+
+  <article className="rounded-xl bg-gradient-to-r from-red-900 via-red-500 to-yellow-300 p-0.5 shadow-xl transition hover:shadow-sm">
+      <div className="bg-white p-4 sm:p-6">
+        <a href="#">
+          <h2 className="font-bold text-xl mt-8 mb-4 text-center">
+        Profesiones y oficios
+        </h2>
+      </a>
+      <ul>
+  <li>Muy creativo y disfruta en profesiones que requieren iniciativa.</li>
+  <li>Destaca en áreas científicas, comerciales, técnicas y artísticas.</li>
+  <li>Puede sobresalir en investigación, biología, física y química.</li>
+  <li>Desarrolla estrategias de marketing innovadoras.</li>
+  <li>Destaca en ingeniería, programación, arquitectura y diseño de proyectos innovadores.</li>
+  <li>Sobresale en el ámbito artístico como artista, diseñador, escritor o músico.</li>
+</ul>
+    </div>
+  </article>
+
+</div>
+</div>
+
+<div className="video-container p-5">
+          <iframe
+            src="https://www.youtube.com/embed/8xY6XnSPecM"
+            title="Video del signo azteca mono"
+            allowFullScreen
+            className="w-full h-96"
+          ></iframe>
+        </div>
+      </article>
+    );
+  };
+  
+  export default Mono;
+  
+  
